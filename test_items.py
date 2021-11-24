5e468bc23b14c6f97fbc216b3f812bf8818d8ca4
AndreySib <30804833+AndreySib@users.noreply.github.com>
2021-11-24 21:47:27 +0500

    Файлы для проверки
    Задание: запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_add_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    browser.find_element_by_css_selector("#add_to_basket_form > button")
