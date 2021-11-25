33ca2ad9179b123fa0ee8e6baafd083be431893a
AndreySib <30804833+AndreySib@users.noreply.github.com>
2021-11-25 22:52:06 +0500

    Итоговый test_items для проверки

--- test_items.py
+++ test_items.py
@@ -1,5 +1,10 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import pytest
+import time
+link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form > button")
+    time.sleep(30) 
+    
+    assert browser.find_element_by_css_selector('#add_to_basket_form > button').is_displayed(), \
+        'Кнопка товар в корзину отсутсвует'
