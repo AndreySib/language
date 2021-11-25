4bcc41a74cb7d70e92da86e0ac4be41fe0dc4f3c
AndreySib <30804833+AndreySib@users.noreply.github.com>
2021-11-25 23:41:33 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_add_basket(browser):
     browser.get(link)
     time.sleep(30) 
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button, "Кнопка товар в корзину отсутсвует"
     
-    assert browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button').is_displayed(), \
-        'Кнопка товар в корзину отсутсвует'
