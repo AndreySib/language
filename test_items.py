26819847285d73ee82c307e5b17da16820608211
AndreySib <30804833+AndreySib@users.noreply.github.com>
2021-11-25 23:03:12 +0500

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ def test_guest_should_see_add_basket(browser):
     browser.get(link)
     time.sleep(30) 
     
-    assert browser.find_element_by_css_selector('#add_to_basket_form > button').is_displayed(), \
+    assert browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button').is_displayed(), \
         'Кнопка товар в корзину отсутсвует'
